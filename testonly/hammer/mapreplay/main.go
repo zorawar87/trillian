ac2e9c487f4f414d37a4468f73d55dacfa3a44a8
Pavel Kalinnikov <pavelkalinnikov@gmail.com>
2019-12-06 11:20:18 +0000

    Revert "Made the hammer internal-only (#2004)"
    This reverts commit eeaecf0648f18d718fbf4fc28cb74b00352f76b3.

--- testonly/hammer/mapreplay/main.go
+++ testonly/hammer/mapreplay/main.go
@@ -25,7 +25,7 @@ import (
 
 	"github.com/golang/glog"
 	"github.com/google/trillian"
-	"github.com/google/trillian/testonly/internal/hammer"
+	"github.com/google/trillian/testonly/hammer"
 	"google.golang.org/grpc"
 )
 
